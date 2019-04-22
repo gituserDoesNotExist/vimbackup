67950baa6019a11af8b9154d71696e73eb8c7980
gituserDoesNotExist <manfred-beutel@web.de>
2019-04-22 21:56:44 +0200

    backup...

--- code_es_metadata/ES_METADATA/package_bodies/user_input_analyzer_test.sql
+++ code_es_metadata/ES_METADATA/package_bodies/user_input_analyzer_test.sql
@@ -1,136 +1,157 @@
 CREATE OR REPLACE PACKAGE BODY ES_METADATA.USER_INPUT_ANALYZER_TEST AS
 
-    result VARCHAR2(30);
-
-    PROCEDURE extract_schema_name AS
-        v_user_input VARCHAR2(30) := 'dietplan.someobject';
-    BEGIN
-        result := user_input_analyzer.extract_schema_name(v_user_input);
-        ut3.ut.expect(result).to_equal('dietplan');
-    END extract_schema_name;
-
-    PROCEDURE extract_schema_name_no_schema AS
-        v_user_input VARCHAR2(30) := 'someobject';
-    BEGIN
-        result := user_input_analyzer.extract_schema_name(v_user_input);
-        ut3.ut.expect(result).to_be_null();
-    END extract_schema_name_no_schema;
-
-    PROCEDURE extract_dbobj_name AS
-        v_user_input VARCHAR2(30) := 'dietplan.someobject';
-    BEGIN
-        result := user_input_analyzer.extract_dbobj_name(v_user_input);
-        ut3.ut.expect(result).to_equal('someobject');
-    END extract_dbobj_name;
-    
-    procedure extract_dbobj_name_all AS
+    extracted_dbobj VARCHAR2(30);
+    extracted_schema VARCHAR2(30);
+    extracted_attr VARCHAR2(30);
+    contains_attr boolean;
+--################################ test extract dbobj start##########################################
+     --%test(testExtractDbObjName_UserInputIs_Schema.DbObjName.Attr_ReturnsDbObjName)
+    procedure extr_all_sch_dbobj_attr  AS
         v_user_input VARCHAR2(30) := 'dietplan.someobject.attr';
     BEGIN
-        result := user_input_analyzer.extract_dbobj_name(v_user_input);
-        ut3.ut.expect(result).to_equal('someobject');
+        extracted_schema := user_input_analyzer.extract_schema_name(v_user_input);
+        ut3.ut.expect(extracted_schema).to_equal('dietplan');
+
+        extracted_dbobj := user_input_analyzer.extract_dbobj_name(v_user_input);
+        ut3.ut.expect(extracted_dbobj).to_equal('someobject');
+        
+        extracted_attr := user_input_analyzer.extract_attr_or_method(v_user_input);
+        ut3.ut.expect(extracted_attr).to_equal('attr');
+        
+        contains_attr := user_input_analyzer.contains_attr_or_method_part(v_user_input);
+        ut3.ut.expect(contains_attr).to_be_true();
     END;
-    
-    procedure extract_dbobj_name_onlyschema  AS
+    --%test(testExtractDbObjName_UserInputIs_Schema.DbObjName._ReturnsDbObjName)
+    procedure extr_all_sch_dbobj_dot  AS
+        v_user_input VARCHAR2(30) := 'dietplan.someobject.';
+    BEGIN
+        extracted_schema := user_input_analyzer.extract_schema_name(v_user_input);
+        ut3.ut.expect(extracted_schema).to_equal('dietplan');
+
+        extracted_dbobj := user_input_analyzer.extract_dbobj_name(v_user_input);
+        ut3.ut.expect(extracted_dbobj).to_equal('someobject');
+        
+        extracted_attr := user_input_analyzer.extract_attr_or_method(v_user_input);
+        ut3.ut.expect(extracted_attr).to_equal('');
+        
+        contains_attr := user_input_analyzer.contains_attr_or_method_part(v_user_input);
+        ut3.ut.expect(contains_attr).to_be_true();
+    END;
+    --%test(testExtractDbObjName_UserInputIs_Schema.DbObjName_ReturnsDbObjName)
+    procedure extr_all_sch_dbobj  AS
+        v_user_input VARCHAR2(30) := 'dietplan.someobject';
+    BEGIN
+        extracted_schema := user_input_analyzer.extract_schema_name(v_user_input);
+        ut3.ut.expect(extracted_schema).to_equal('dietplan');
+
+        extracted_dbobj := user_input_analyzer.extract_dbobj_name(v_user_input);
+        ut3.ut.expect(extracted_dbobj).to_equal('someobject');
+        
+        extracted_attr := user_input_analyzer.extract_attr_or_method(v_user_input);
+        ut3.ut.expect(extracted_attr).to_equal('');
+        
+        contains_attr := user_input_analyzer.contains_attr_or_method_part(v_user_input);
+        ut3.ut.expect(contains_attr).to_be_false();
+    END;
+    --%test(testExtractDbObjName_UserInputIs_Schema._ReturnsDbObjName)
+    procedure extr_all_schema_dot  AS
         v_user_input VARCHAR2(30) := 'dietplan.';
     BEGIN
-        result := user_input_analyzer.extract_dbobj_name(v_user_input);
-        ut3.ut.expect(result).to_equal('');
-    END extract_dbobj_name_onlyschema;
+        extracted_schema := user_input_analyzer.extract_schema_name(v_user_input);
+        ut3.ut.expect(extracted_schema).to_equal('dietplan');
 
-    PROCEDURE extract_dbobj_name_no_schema IS
-        v_user_input VARCHAR2(30) := 'someobject';
+        extracted_dbobj := user_input_analyzer.extract_dbobj_name(v_user_input);
+        ut3.ut.expect(extracted_dbobj).to_equal('');
+        
+        extracted_attr := user_input_analyzer.extract_attr_or_method(v_user_input);
+        ut3.ut.expect(extracted_attr).to_equal('');
+        
+        contains_attr := user_input_analyzer.contains_attr_or_method_part(v_user_input);
+        ut3.ut.expect(contains_attr).to_be_false();
+    END;
+    --%test(testExtractDbObjName_UserInputIs_Schema_ReturnsDbObjName)
+    procedure extr_all_schema  AS
+        v_user_input VARCHAR2(30) := 'dietplan';
     BEGIN
-        result := user_input_analyzer.extract_dbobj_name(v_user_input);
-        ut3.ut.expect(result).to_equal('someobject');
-    END extract_dbobj_name_no_schema;
+        extracted_schema := user_input_analyzer.extract_schema_name(v_user_input);
+        ut3.ut.expect(extracted_schema).to_equal('dietplan');
 
-    procedure extract_dbobj_name_attr 
-    IS
+        extracted_dbobj := user_input_analyzer.extract_dbobj_name(v_user_input);
+        ut3.ut.expect(extracted_dbobj).to_equal('');
+        
+        extracted_attr := user_input_analyzer.extract_attr_or_method(v_user_input);
+        ut3.ut.expect(extracted_attr).to_equal('');
+        
+        contains_attr := user_input_analyzer.contains_attr_or_method_part(v_user_input);
+        ut3.ut.expect(contains_attr).to_be_false();
+    END;
+    --%test(testExtractDbObjName_UserInputIs_DbObj.attr_ReturnsDbObjName)
+    procedure extr_all_dbobj_attr  AS
         v_user_input VARCHAR2(30) := 'someobject.attr';
     BEGIN
-        result := user_input_analyzer.extract_dbobj_name(v_user_input);
-        ut3.ut.expect(result).to_equal('someobject');
-    END extract_dbobj_name_attr;
+        extracted_schema := user_input_analyzer.extract_schema_name(v_user_input);
+        ut3.ut.expect(extracted_schema).to_equal('');
 
-
-    PROCEDURE extract_attr_or_method AS
-        v_user_input VARCHAR2(30) := 'dietplan.someobject.someattr';
-    BEGIN
-        result := user_input_analyzer.extract_attr_or_method(v_user_input);
-        ut3.ut.expect(result).to_equal('someattr');
-    END extract_attr_or_method;
-
-    procedure extract_attrormethod_no_schema as
-        v_user_input VARCHAR2(30) := 'someobject.someattr';
-    BEGIN
-        result := user_input_analyzer.extract_attr_or_method(v_user_input);
-        ut3.ut.expect(result).to_equal('someattr');
-    END extract_attrormethod_no_schema;
-
-    procedure extract_attrormethod_only_dot as
+        extracted_dbobj := user_input_analyzer.extract_dbobj_name(v_user_input);
+        ut3.ut.expect(extracted_dbobj).to_equal('someobject');
+        
+        extracted_attr := user_input_analyzer.extract_attr_or_method(v_user_input);
+        ut3.ut.expect(extracted_attr).to_equal('attr');
+        
+        contains_attr := user_input_analyzer.contains_attr_or_method_part(v_user_input);
+        ut3.ut.expect(contains_attr).to_be_true();
+    END;
+    --%test(testExtractDbObjName_UserInputIs_DbObj._ReturnsDbObjName)
+    procedure extr_all_dbobj_dot  AS
         v_user_input VARCHAR2(30) := 'someobject.';
     BEGIN
-        result := user_input_analyzer.extract_attr_or_method(v_user_input);
-        ut3.ut.expect(result).to_equal('');
-    END extract_attrormethod_only_dot ;
+        extracted_schema := user_input_analyzer.extract_schema_name(v_user_input);
+        ut3.ut.expect(extracted_schema).to_equal('');
 
-    procedure extract_attr_only_dbobj_name as
+        extracted_dbobj := user_input_analyzer.extract_dbobj_name(v_user_input);
+        ut3.ut.expect(extracted_dbobj).to_equal('someobject');
+        
+        extracted_attr := user_input_analyzer.extract_attr_or_method(v_user_input);
+        ut3.ut.expect(extracted_attr).to_equal('');
+        
+        contains_attr := user_input_analyzer.contains_attr_or_method_part(v_user_input);
+        ut3.ut.expect(contains_attr).to_be_true();
+    END;
+    --%test(testExtractDbObjName_UserInputIs_DbObj_ReturnsDbObjName)
+    procedure extr_all_dbobj  AS
         v_user_input VARCHAR2(30) := 'someobject';
     BEGIN
-        result := user_input_analyzer.extract_attr_or_method(v_user_input);
-        ut3.ut.expect(result).to_equal('');
-    END extract_attr_only_dbobj_name;
+        extracted_schema := user_input_analyzer.extract_schema_name(v_user_input);
+        ut3.ut.expect(extracted_schema).to_equal('');
 
-    procedure extract_attr_only_schemadbobj is
-        v_user_input VARCHAR2(30) := 'sys.someobjectpart';
-    BEGIN
-        result := user_input_analyzer.extract_attr_or_method(v_user_input);
-        ut3.ut.expect(result).to_equal('');
-    END extract_attr_only_schemadbobj;
-
-
-    PROCEDURE contains_attrpart_schmea_db AS
-        v_user_input VARCHAR2(30) := 'dietplan.someobject';
-        contains_attr boolean;
-    BEGIN
+        extracted_dbobj := user_input_analyzer.extract_dbobj_name(v_user_input);
+        ut3.ut.expect(extracted_dbobj).to_equal('someobject');
+        
+        extracted_attr := user_input_analyzer.extract_attr_or_method(v_user_input);
+        ut3.ut.expect(extracted_attr).to_equal('');
+        
         contains_attr := user_input_analyzer.contains_attr_or_method_part(v_user_input);
         ut3.ut.expect(contains_attr).to_be_false();
-    END contains_attrpart_schmea_db;
-
-    PROCEDURE contains_attrpart_db_attr AS
-        v_user_input VARCHAR2(30) := 'someobject.attr';
-        contains_attr boolean;
+    END;
+    --%test(testExtractDbObjName_UserInputIsEmpty_ReturnsDbObjName)
+    procedure extr_all_no_input  AS
+        v_user_input VARCHAR2(30) := '';
     BEGIN
-        contains_attr := user_input_analyzer.contains_attr_or_method_part(v_user_input);
-        ut3.ut.expect(contains_attr).to_be_true();
-    END contains_attrpart_db_attr;
+        extracted_schema := user_input_analyzer.extract_schema_name(v_user_input);
+        ut3.ut.expect(extracted_schema).to_equal('');
 
-    PROCEDURE contains_attrpart_schma_db_att AS
-        v_user_input VARCHAR2(30) := 'dietplan.someobject.attr';
-        contains_attr boolean;
-    BEGIN
-        contains_attr := user_input_analyzer.contains_attr_or_method_part(v_user_input);
-        ut3.ut.expect(contains_attr).to_be_true();
-    END contains_attrpart_schma_db_att;
-
-
-    PROCEDURE contains_attrpart_only_dot AS
-        v_user_input VARCHAR2(30) := 'dbobj.';
-        contains_attr boolean;
-    BEGIN
-        contains_attr := user_input_analyzer.contains_attr_or_method_part(v_user_input);
-        ut3.ut.expect(contains_attr).to_be_true();
-    END contains_attrpart_only_dot;
-
-
-    procedure contns_attrormethod_onlyschema as
-        v_user_input VARCHAR2(30) := 'dietplan.';
-        contains_attr boolean;
-    BEGIN
+        extracted_dbobj := user_input_analyzer.extract_dbobj_name(v_user_input);
+        ut3.ut.expect(extracted_dbobj).to_equal('');
+        
+        extracted_attr := user_input_analyzer.extract_attr_or_method(v_user_input);
+        ut3.ut.expect(extracted_attr).to_equal('');
+        
         contains_attr := user_input_analyzer.contains_attr_or_method_part(v_user_input);
         ut3.ut.expect(contains_attr).to_be_false();
-    END contns_attrormethod_onlyschema;
+    END;
+--################################ test extract dbobj end##########################################
 
+  
 
 
 
--- code_es_metadata/ES_METADATA/packages/user_input_analyzer_test.sql
+++ code_es_metadata/ES_METADATA/packages/user_input_analyzer_test.sql
@@ -1,65 +1,28 @@
 CREATE OR REPLACE PACKAGE ES_METADATA.USER_INPUT_ANALYZER_TEST AS 
 
     --%suite(test for user input parser)
-    
-    --%test(extractSchemaName_UserInputContainsSchemaName_ReturnsSchemaName)
-    procedure extract_schema_name;
-    
-    --%test(extractSchemaName_UserInputDoesNotContainSchemaName_ReturnsEmptyString)
-    procedure extract_schema_name_no_schema;
-       
-    --%test(testExtractDbObjName_UserInputIs_Schema.DbObjName_ReturnsDbObjName)
-    procedure extract_dbobj_name;
 
+    --usecases: schema.dbobj.attr or schema.dbobj. or schema.dbobj or schema. or schema
+    --          dbobj.attr or dbobj. or dbobj
+    --          no input
     --%test(testExtractDbObjName_UserInputIs_Schema.DbObjName.Attr_ReturnsDbObjName)
-    procedure extract_dbobj_name_all;
-
-
-    --%test(testExtractDbObjName_UserInputIs_Schema._ReturnsEmptyDbObjName)
-    procedure extract_dbobj_name_onlyschema;
-
-
-    --%test(testExtractDbObjName_UserInputIs_DbObjName_ReturnsDbObjName)
-    procedure extract_dbobj_name_no_schema;
-
-    --%test(testExtractDbObjName_UserInputIs_DbObjName.Attr_ReturnsDbObjName)
-    procedure extract_dbobj_name_attr;
-    
-    --%test(testExtractAttrOrMethod_UserInputIs_Schema.DbObjName.Attr_ReturnsAttr)
-    procedure extract_attr_or_method;
-    
-    --%test(testExtractAttrOrMethod_UserInputIs_DbObjName.Attr_ReturnsAttr)
-    procedure extract_attrormethod_no_schema;
-
-
-    --%test(testExtractAttrOrMethod_UserInputIs_DbObjName._ReturnsAttr)
-    procedure extract_attrormethod_only_dot;
- 
-    --%test(testExtractAttrOrMethod_UserInputIs_DbObjName_DoesNotContainAttr_ReturnsNull)
-    procedure extract_attr_only_dbobj_name;
- 
- 
-    --%test(testExtractAttrOrMethod_UserInputIs_Schema.DbObjName_DoesNotContainAttr_ReturnsNull)
-    procedure extract_attr_only_schemadbobj;
-       
-    
-    --%test(testContainsAttrOrMethodPart_UserInputIs_Schema.DbObjName_ReturnsFalse)
-    procedure contains_attrpart_schmea_db;
-
-    --%test(testContainsAttrOrMethodPart_UserInputIs_DbObjName.Attr_ReturnsTrue)
-    procedure contains_attrpart_db_attr;
-    
-    
-    --%test(testContainsAttrOrMethodPart_UserInputIs_Schema.DbObjName.Attr_ReturnsTrue)
-    procedure contains_attrpart_schma_db_att;
-
-    --%test(testContainsAttrOrMethodPart_UserInputIs_DbObjName._ReturnsTrue)
-    procedure contains_attrpart_only_dot;
-
-
-    --%test(testExtractAttrOrMethod_UserInputIs_Schemaname._ReturnsAttr)
-    procedure contns_attrormethod_onlyschema;
-
+    procedure extr_all_sch_dbobj_attr;
+    --%test(testExtractDbObjName_UserInputIs_Schema.DbObjName._ReturnsDbObjName)
+    procedure extr_all_sch_dbobj_dot;
+    --%test(testExtractDbObjName_UserInputIs_Schema.DbObjName_ReturnsDbObjName)
+    procedure extr_all_sch_dbobj;
+    --%test(testExtractDbObjName_UserInputIs_Schema._ReturnsDbObjName)
+    procedure extr_all_schema_dot;
+    --%test(testExtractDbObjName_UserInputIs_Schema_ReturnsDbObjName)
+    procedure extr_all_schema;
+    --%test(testExtractDbObjName_UserInputIs_DbObj.attr_ReturnsDbObjName)
+    procedure extr_all_dbobj_attr;
+    --%test(testExtractDbObjName_UserInputIs_DbObj._ReturnsDbObjName)
+    procedure extr_all_dbobj_dot;
+    --%test(testExtractDbObjName_UserInputIs_DbObj_ReturnsDbObjName)
+    procedure extr_all_dbobj;
+    --%test(testExtractDbObjName_UserInputIsEmpty_ReturnsDbObjName)
+    procedure extr_all_no_input;
 
 
 END USER_INPUT_ANALYZER_TEST;
